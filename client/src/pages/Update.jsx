a32fc48dcefa2b91404503d9f94dbfc05c2ba710
Justin Hadinata <Justinhadinata.23@gmail.com>
2024-11-30 14:55:11 +0700

    Add comments to unnecessary files.

--- client/src/pages/Update.jsx
+++ client/src/pages/Update.jsx
@@ -1,64 +1,64 @@
-import axios from "axios";
-import { useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+// import axios from "axios";
+// import { useState } from "react";
+// import { useNavigate, useLocation } from "react-router-dom";
 
-function Update() {
-  const [book, setBook] = useState({
-    title: "",
-    desc: "",
-    price: null,
-    cover: "",
-  });
+// function Update() {
+//   const [book, setBook] = useState({
+//     title: "",
+//     desc: "",
+//     price: null,
+//     cover: "",
+//   });
 
-  const location = useLocation();
-  const bookId = location.pathname.split("/")[2];
+//   const location = useLocation();
+//   const bookId = location.pathname.split("/")[2];
 
-  const navigate = useNavigate();
+//   const navigate = useNavigate();
 
-  function handleChange(e) {
-    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  }
-  async function handleClick(e) {
-    e.preventDefault();
-    try {
-      await axios.put("http://localhost:8800/books/" + bookId, book);
-      navigate("/");
-    } catch (err) {
-      console.log(err);
-    }
-  }
-  return (
-    <div className="form">
-      <h1>Update the Book</h1>
-      <input
-        type="text"
-        placeholder="title"
-        onChange={handleChange}
-        name="title"
-      />
-      <input
-        type="text"
-        placeholder="desc"
-        onChange={handleChange}
-        name="desc"
-      />
-      <input
-        type="number"
-        placeholder="price"
-        onChange={handleChange}
-        name="price"
-      />
-      <input
-        type="text"
-        placeholder="cover"
-        onChange={handleChange}
-        name="cover"
-      />
-      <button onClick={handleClick} className="formButton">
-        Update
-      </button>
-    </div>
-  );
-}
+//   function handleChange(e) {
+//     setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+//   }
+//   async function handleClick(e) {
+//     e.preventDefault();
+//     try {
+//       await axios.put("http://localhost:8800/books/" + bookId, book);
+//       navigate("/");
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   }
+//   return (
+//     <div className="form">
+//       <h1>Update the Book</h1>
+//       <input
+//         type="text"
+//         placeholder="title"
+//         onChange={handleChange}
+//         name="title"
+//       />
+//       <input
+//         type="text"
+//         placeholder="desc"
+//         onChange={handleChange}
+//         name="desc"
+//       />
+//       <input
+//         type="number"
+//         placeholder="price"
+//         onChange={handleChange}
+//         name="price"
+//       />
+//       <input
+//         type="text"
+//         placeholder="cover"
+//         onChange={handleChange}
+//         name="cover"
+//       />
+//       <button onClick={handleClick} className="formButton">
+//         Update
+//       </button>
+//     </div>
+//   );
+// }
 
-export default Update;
+// export default Update;
